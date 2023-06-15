53bdd9587892af3e646d1aa810d759bfe042e3c5
asaqa988 <asaqa001@gmail.com>
2023-06-15 11:31:45 +0300

    update2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import "./App.css";
-import MyFooter from './componentes/footer' 
-import MyHeader from './componentes/header'
-import MyMain from './componentes/main'
+import MyFooter from "./componentes/footer";
+import MyHeader from "./componentes/header";
+import MyMain from "./componentes/main";
 import HashemComponent from "./componentes/hashem";
-import MansafffffComponent from "./componentes/mansaf";
+import MyData from "./componentes/testMap";
 
 function App() {
-  return <div className="App">
-<MyHeader/>
-<MyMain/>
-<MyFooter/>
-<HashemComponent/>
-
-<MansafffffComponent/>
-<MansafffffComponent/>
-<MansafffffComponent/>
-<MansafffffComponent/>
-<MansafffffComponent/>
-<MansafffffComponent/>
-<MansafffffComponent/><MansafffffComponent/>
-<MansafffffComponent/>
-<MansafffffComponent/>
-<MansafffffComponent/>
-
-  </div>;
+  return (
+    <div className="App">
+      {/* <MyHeader />
+      <MyMain />
+      <MyFooter />
+      <HashemComponent /> */}
+      <MyData />
+    </div>
+  );
 }
 
-export default App
+export default App;
